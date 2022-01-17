a4f9736124f4cf0effff28c5f6291408c896feb2
surajit0609 <mandalsurajit.0609@gmail.com>
2022-01-18 02:24:37 +0530

    interview

---
+++ Loop solution/pattern loop solution/Q18.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		for(j=n;j>i;j--){
+			printf("*");
+		}
+		for(j=0;j<2*i;j++){
+			printf(" ");
+		}
+		for(j=n;j>i;j--){
+		
+		printf("*");
+		}
+		printf("\n");
+	}
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+			printf("*");
+		}
+		for(j=1;j<=2*n-2*i;j++){
+			printf(" ");
+		}
+		for(j=1;j<=i;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
